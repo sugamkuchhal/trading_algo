8993b1d3bdd78b4381da6af19e2335540ca5f715
sugamkuchhal <sugamkuchhal@gmail.com>
2025-11-01 18:20:05 +0530

    Delete trading_algo/trading_algo directory

--- trading_algo/trading_algo/utils.py
+++
@@ -1,9 +0,0 @@
-import logging
-logger = logging.getLogger(__name__)
-
-def safe_run(func, *args, **kwargs):
-    try:
-        return func(*args, **kwargs)
-    except Exception:
-        logger.exception("Error during safe_run")
-        return None
